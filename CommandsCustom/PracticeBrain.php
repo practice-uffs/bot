dd50169a60b4c01488d99b57cfe15550069e2cad
Fernando Bevilacqua <dovyski@gmail.com>
2021-01-11 15:30:19 -0300

    feat: estrutura para processamento de conteúdo

--- CommandsCustom/PracticeBrain.php
+++ CommandsCustom/PracticeBrain.php
@@ -10,15 +10,48 @@
 
 use Longman\TelegramBot\Commands\SystemCommand;
 use Longman\TelegramBot\Entities\ServerResponse;
+use Longman\TelegramBot\Request;
 
 class PracticerBrain
 {
+    protected SystemCommand $sys;
+    protected $message;
+    protected $sender;
+
+    public function __construct()
+    {
+    }
+
+    protected function giveIssueInfo()
+    {
+        $message_type = $this->message->getType();
+
+        if($message_type == 'text') {
+            return $this->sys->replyToChat("new text not found.. :(");
+        }
+
+        return null;
+    }
+
+    protected function handleCommand()
+    {
+        $user_id = $this->message->getFrom()->getId();
+        $command = $this->message->getCommand();
+    }
+
     public function run(SystemCommand $cmd): ServerResponse
     {
-        $message = $cmd->getMessage();
-        $user_id = $message->getFrom()->getId();
-        $command = $message->getCommand();
+        $this->sys = $cmd;
+        $this->message = $cmd->getMessage();
+        $this->user = $this->message->getFrom();
 
-        return $cmd->replyToChat("new text not found.. :(");
+        // Any of the following methods will return a result if they want to
+        // stop the chaining of other methods, otherwise everything wi be checked.
+
+        if($result = $this->handleCommand()) { return $result; }
+        if($result = $this->giveIssueInfo()) { return $result; }
+
+        // If we got here, we have no action to reply...
+        return Request::emptyResponse();
     }
 }

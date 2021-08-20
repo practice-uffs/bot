c4a4d84f1ee47b62b516fb27b8c4be85cdb7c3c9
Fernando Bevilacqua <dovyski@gmail.com>
2021-08-20 15:14:46 -0300

    feat: mensagem de aniversário \o/

--- app/Commands/cron.php
+++ app/Commands/cron.php
@@ -1,33 +1,35 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../telegram/Telegram.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../../telegram/Telegram.php';
 
 use Treinetic\ImageArtist\lib\Image;
 
-$config = require __DIR__ . '/../config/config.php';
+$config = require __DIR__ . '/../../config/config.php';
 
 try {
+    $assetsDir = __DIR__ . '/../../resources/assets/';
     $telegram = new Telegram($config);
-    //$reponse = $telegram->sendMessage('-1001252002318', 'https://www.carspecs.us/photos/c8447c97e355f462368178b3518367824a757327-2000.jpg');
+    $chatId = '-1001315997119';
 
-    $person = new Image(__DIR__ . '/fernando-bevilacqua.png');
-    $confetti = new Image(__DIR__ . '/confetti.png');
-    $hat = new Image(__DIR__ . '/hat.png');
+    $person = new Image($assetsDir . 'fernando-bevilacqua.jpg');
+    $confetti = new Image($assetsDir . 'confetti.png');
+    $hat = new Image($assetsDir . 'hat.png');
     
     $hat->scaleToWidth($person->getWidth() * 0.25);
     $hat->rotate(-40);
     $confetti->resize($person->getWidth() * 1.1, $person->getHeight() * 1.1);
     
     $person->merge($confetti, ($person->getWidth() - $confetti->getWidth()) / 2, ($person->getHeight() - $confetti->getHeight()) / 2);
-    $person->merge($hat, $person->getWidth() - $hat->getWidth() - 10, -50);
+    $person->merge($hat, $person->getWidth() - $hat->getWidth() - 10, -90);
 
-    $person->save(__DIR__ . '/out.png',IMAGETYPE_PNG);
+    $tempImagePath = sys_get_temp_dir() . 'out.png';
+    $person->save($tempImagePath,IMAGETYPE_PNG);
 
-    $photo = fopen(__DIR__ . '/out.png', 'r');
-    $reponse = $telegram->sendPhoto('-1001252002318', $photo, 'user.png');
+    $photo = fopen($tempImagePath, 'r');
+    $response = $telegram->sendPhoto($chatId, $photo, '');
 
-    if ($reponse->getStatusCode() != 200) {
+    if ($response->getStatusCode() != 200) {
         echo 'Hum, something wrong' . PHP_EOL;
     }
 
